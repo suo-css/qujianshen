a817b8398bcc5f447583b30c4a340384e2bbcdc2
zhai-php <455078389@qq.com>
2014-07-15 12:06:34 +0800

    revise the password for database

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -42,7 +42,7 @@ return array(
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'jianshen', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'Wnwx0625',  // 密码
+    'DB_PWD'    => '',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
